5cfa767cec4d91e5f57e840d28b24d5578588146
rogarfil <rogarfil@yahoo.com.br>
2020-12-27 22:59:05 -0300

    Library Ok

--- _app/Email.php
+++ _app/Email.php
@@ -9,21 +9,21 @@ class Email
 {
     private $mail = \stdClass::class;
 
-    public  function __construct()
+    public  function __construct($smtpDebug, $host, $user, $pass, $smtpSecure, $port, $setFromEmail, $setFromName)
     {
         $this->mail = new PHPMailer(true);
-        $this->mail->SMTPDebug = 2;                      // Enable verbose debug output
-        $this->mail->isSMTP();                                            // Send using SMTP
-        $this->mail->Host       = 'smtp.gmail.com';                     // Set the SMTP server to send through
-        $this->mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-        $this->mail->Username   = 'rogarfil@gmail.com';                     // SMTP username
-        $this->mail->Password   = '2r0g1f3!@';                               // SMTP password
-        $this->mail->SMTPSecure = 'tls';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-        $this->mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+        $this->mail->SMTPDebug = $smtpDebug;           // Enable verbose debug output
+        $this->mail->isSMTP();                         // Send using SMTP
+        $this->mail->Host       = $host;               // Set the SMTP server to send through
+        $this->mail->SMTPAuth   = true;                // Enable SMTP authentication
+        $this->mail->Username   = $user;               // SMTP username
+        $this->mail->Password   = $pass;               // SMTP password
+        $this->mail->SMTPSecure = $smtpSecure;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+        $this->mail->Port       = $port;               // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
         $this->mail->CharSet = 'utf-8';
         $this->mail->setLanguage('br');
         $this->mail->isHTML(true);
-        $this->mail->setFrom('canutampp@gmail.com', 'Equipe GustavoWeb');
+        $this->mail->setFrom($setFromEmail, $setFromName);
     }
 
     public function sendMail($subject, $body, $replyEmail, $replyName, $addressEmail, $addressName)

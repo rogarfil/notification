c3196ae60865a1d27eeb1ddb9d10496a51b59679
rogarfil <rogarfil@yahoo.com.br>
2020-12-28 13:45:07 -0300

    Test fix

--- test/index.php
+++ test/index.php
@@ -4,10 +4,8 @@ require __DIR__ . '/../lib_ext/autoload.php';
 
 use Notification\Email;
 
-//var_dump($email);
+$email = new Email(2, "mail.host.com", "your@email.com", "your-pass", "smtp secure (tls/ssl)", "port (587)", "from@email.com", "From Name");
 
-$novoEmail = new Email(2, "smtp.gmail.com", "rxxxxxxl@gmail.com", "r1o2g3a4r", "tls",
-    "587", "cxxxxxxxp@gmail.com", "Equipe Gustavo Web");
-$novoEmail->sendMail("Assunto de Teste", "<p>Esse é um e-mail de <b>teste</b>!</p>", "cxxxxxxxp@gmail.com", "Gustavo Web", "rxxxxxxl@hotmail.com", "Gustavo");
+$email->sendEmail("Subject", "Content", "reply@email.com", "Replay Name", "address@email.com", "Address Name");
 
-var_dump($novoEmail);
+var_dump($email);
